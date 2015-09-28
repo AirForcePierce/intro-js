98e8d19369444da8583223e01b72f88e1a80e9f5
Jontae Pierce <jontae.pierce@gmail.com>
2015-09-28 13:28:14 -0400

    Added answers to main.js

--- main.js
+++ main.js
@@ -3,22 +3,22 @@
 */
 var givenName;
 // Q: What does `givenName` equal right now?
-// A:
+// A: undefined
 
 
 givenName = "Tim";
 // Q: What is `givenName` set to right now?
-// A:
+// A:"Tim" (the string)
 
 
 givenName = "JD";
 // Q: What is `givenName` set to right now?
-// A:
+// A: "JD" (the string)
 
 
 var greeting = "Hello, how are you?";
 // Q: What is `greeting` set to?
-// A:
+// A: "Hello, how are you?" (the string)
 
 
 /*=========================================================
@@ -31,11 +31,11 @@ var low  = 10;
 
 var math = high - low;
 // Q: What is `math` set to?
-// A:
+// A: Math is set to 40
 
 math = high - "5";
 // Q: What is `math` set to?
-// A:
+// A: 45
 
 
 /* =========================================================
@@ -44,17 +44,19 @@ math = high - "5";
 
 // Create a variable to calculate Tim's age
 // The answer should read "Tim is 32 years old"
-// The answer shoud not be written in a comment.
+// The answer should not be written in a comment.
 var born = 1983;
 var today = 2015;
 
 // Answers Below:
-
+var howOld = today - born;
+var birthdayMsg = "Tim is " + howOld + " years old";
+console.log (birthdayMsg);
 
 
 // Store some information following in variables.
-var yourName;
-var instructorName;
+var yourName = "Jontae";
+var instructorName = "Tim";
 
 // Update the variables above so the expression reads correctly.
 // Answers Below:
